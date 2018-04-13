f22aa25ff9a7ce03d8a264108a23314afb0990d8
Hilal Yıldırım <32734377+hilal-12@users.noreply.github.com>
2018-04-13 12:05:57 +0300

    Add files via upload

---
+++ kurumsal/ustbilgi.js
@@ -0,0 +1,33 @@
+var menu=
+    '<ul class="navbar-nav mr-auto">'+
+    '<li class="nav-item active">'+
+    '<a class="nav-link" href="kurumsal-bilgi-y%C3%B6netimi.html">Kurumsal Bilgi Yönetimi <span class="sr-only">(current)</span></a></li>'+
+    '<li class="nav-item dropdown active">'+
+    '<a class="nav-link dropdown-toggle" href="kurumsal-içerik-yönetimi.html" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kurumsal İçerik Yönetimi</a>'+
+    '<div class="dropdown-menu" aria-labelledby="dropdown01">'+
+    '<a class="dropdown-item" href="icerik-yönetiminin-kapsamı.html">İçerik Yönetiminin Kapsamı</a>'+
+    '<a class="dropdown-item" href="kurum-içerik-yönetim-ilişkisi.html">Kurum – İçerik Yönetimi İlişkisi </a>'+
+'</div>'+
+'</li>'+
+    '<li class="nav-item dropdown active">'+
+    '<a class="nav-link dropdown-toggle" href="kiy-kavramı.html" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">KİY Kavramı</a>'+
+    '<div class="dropdown-menu" aria-labelledby="dropdown01">'+
+    '<a class="dropdown-item" href="kiy-gelişimi.html">KİY’in Gelişimi</a>'+
+    '<a class="dropdown-item" href="kiy-işlevleri.html"> KİYin İşlevleri</a>'+
+    '<a class="dropdown-item" href="yapısal-açıdan-kiy.html">Yapısal Açıdan KİY</a>'+
+    '<a class="dropdown-item" href="kiy-süreçleri.html"> KİY Süreçleri</a><a class="dropdown-item" href="kiy-bileşenleri.html"> KİY Bileşenleri  </a>'+
+'</div>'+
+'</li>'+
+    '<li class="nav-item dropdown active">'+
+    '<a class="nav-link dropdown-toggle" href="kiy-modelleri.html" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kurumsal Bilgi Yönetim Modelleri</a>'+
+    '<div class="dropdown-menu" aria-labelledby="dropdown01">'+
+    '<a class="dropdown-item" href="temel-bilgi-yönetim-modelleri.html">Temel Bilgi Yönetim Modeli</a>'+
+    '<a class="dropdown-item" href="cen-ıssn-modeli.html">CEN/ISSS Kurumsal Bilgi Yönetimi Modeli</a>'+
+    '<a class="dropdown-item" href="kbym-modeli.html">KBYM Kurumsal Bilgi Yönetimi Modeli</a>'+
+    '<a class="dropdown-item" href="yaşam-döngüsü-modeli.html">Yaşam Döngüsü Bilgi Yönetimi Modeli</a>'+
+    '<a class="dropdown-item" href="modellerin-değerlendirilmesi.html"> Kurumsal Bilgi Yönetimi Modellerinin Değerlendirilmesi </a>'+
+'</div>'+
+'</li>'+
+'</ul>';
+
+var ayak='<p>© KİY Araştırma Kurumu 2018</p>';
